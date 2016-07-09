568dafd639a41f110fc908246732aaab7978a2cc
Barrie Hadfield <barrie@workshare.com>
2016-07-09 11:01:27 +0100

    changed root to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'navbar' => 'home#navbar'
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#navbar'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
