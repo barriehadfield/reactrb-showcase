96931f0f487b3fe5d8de24453e5872033c9c77a5
Barrie Hadfield <barrie@workshare.com>
2016-08-27 15:40:54 +0100

    fix

--- config/initializers/synchromesh.rb
+++ config/initializers/synchromesh.rb
@@ -6,13 +6,4 @@ Synchromesh.configuration do |config|
     seconds_polled_data_will_be_retained: 1.hour
   }
 
-  # config.transport = :pusher
-  # config.opts = {
-  #   app_id: '233769',
-  #   key:    '36dc6310d01189a720ab',
-  #   secret: '70aa2b879636659c92d7',
-  #   encrypted: false
-  # }
-  # config.channel_prefix = 'syncromesh'
-
 end
